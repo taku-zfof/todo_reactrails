3cd99f24e066d42c6751b04f42972725ff03ab49
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-04-03 23:59:59 +0900

    AddTodoのcss記述まで

--- app/javascript/components/AddTodo.js
+++ app/javascript/components/AddTodo.js
@@ -1,4 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { FiSend } from 'react-icons/fi'
+
+const InputAndButton = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+`
+
+const InputName = styled.input`
+  font-size: 20px;
+  width: 100%;
+  height: 40px;
+  padding: 2px 7px;
+`
+
+const Button = styled.button`
+  font-size: 20px;
+  border: none;
+  border-radius: 3px;
+  margin-left: 10px;
+  padding: 2px 10px;
+  background: #1E90FF;
+  color: #fff;
+  text-align: center;
+  cursor: pointer;
+  ${({ disabled }) => disabled && `
+    opacity: 0.5;
+    cursor: default;
+  `}
+`
+
+const Icon = styled.span`
+  display: flex;
+  align-items: center;
+  margin: 0 7px;
+`
+
+
 
 function AddTodo(){
   return(
